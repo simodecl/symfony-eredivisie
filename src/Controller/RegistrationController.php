bf7a4d0371a0baf06be7de55f6f1531c34cbeb96
Simon Decloedt <simon@3sign.com>
2024-04-25 18:22:18 +0200

    Homepage setup.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -46,7 +46,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('decloedt.simon@gmail.com', 'Symfony Eredivisie'))
+                    ->from(new Address('decloedt.simon@gmail.com', 'Eredivisie Dashboard'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -76,7 +76,6 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_register');
         }
 
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
         return $this->redirectToRoute('app_register');
